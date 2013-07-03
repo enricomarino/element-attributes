441acbfed2061b9dc41504fc1721dffffb4c2f66
Enrico Marino <enrico.marino@email.com>
2013-07-03 18:40:31 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,41 +1,46 @@
-
-/**
- * attr
- * Get attribute `name` 
- * or set attribute `name` to `val`.
- *
- * @param {String} name
- * @param {String} [val]
- * @return {String|Element} attribute value 
- *   or this for chaining
- * @api public
- */
-
-exports.attr = function (name, val) {
-  if (1 == arguments.length) {
-    return this.el.getAttribute(name);
-  } 
-  this.el.setAttribute(name, val);
-  return this;
-};
-
-/**
- * attrs
- * Set attributes given by `obj`
- * 
- * @param {Object} obj 
- * @return {Element} this for chaining
- * @api public
- */ 
-
-exports.attrs = function (obj) {
-  var name;
-  var val;
+module.exports = function (element) {
   
-  for (name in obj) {
-    val = obj[name];
-    this.attr(name, val);
-  }
+  /**
+   * attribute, alias attr
+   * Get attribute `name` 
+   * or set attribute `name` to `val`.
+   *
+   * @param {String} name
+   * @param {String} [val]
+   * @return {String|Element} attribute value 
+   *   or this for chaining
+   * @api public
+   */
   
-  return this;
+  element.prototype.attr = 
+  element.prototype.attribute = function (name, value) {
+    if (1 == arguments.length) {
+      return this.el.getAttribute(name);
+    } 
+    this.el.setAttribute(name, value);
+    return this;
+  };
+  
+  /**
+   * attrs
+   * Set attributes given by `obj`
+   * 
+   * @param {Object} obj 
+   * @return {Element} this for chaining
+   * @api public
+   */ 
+  
+  element.prototype.attrs = function (obj) {
+    var name;
+    var val;
+    
+    for (name in obj) {
+      val = obj[name];
+      this.attr(name, val);
+    }
+    
+    return this;
+  };
+
+  return element;
 };
